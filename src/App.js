4acf872db7f3b8ad04fcbd5d865de28832f4b5de
Matt <mpuent18@gmail.com>
2021-06-07 13:20:20 -0400

    Added filter functionality for tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import ToDo from "./components/Todo";
 import Form from "./components/Form";
-import Filter from "./components/Filter";
+import FilterButton from "./components/FilterButton"
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.complete
+}
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task =>{
@@ -21,7 +28,9 @@ function App(props) {
 
 
   //why does the ToDo component props need to match the task.attribute name?
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
   <ToDo 
   id={task.id} 
   name={task.name}
@@ -31,6 +40,15 @@ function App(props) {
   deleteTask={deleteTask}
   editTask={editTask}  
   />
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+       />
   ));
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
@@ -64,7 +82,7 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <Filter />
+      {filterList}
       <h2 id="list-heading">
         {headingText}
       </h2>
